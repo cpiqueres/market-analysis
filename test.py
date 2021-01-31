4749d2bea3e070ed56e826860acdf4901c633f9c
Carlos <carlos.piqueres@gmail.com>
2021-01-31 20:41:58 +0100

    test eod endpoint with 1 example

--- test.py
+++ test.py
@@ -15,12 +15,26 @@ params = {'access_key':key}
 
 # call the ccy for tickers
 ############
-paramsTicker = {'search':'PHM.BMEX', 'exchange':'BMEX'}
-# add the key
-paramsTicker['access_key'] = key
+def tickers():
+    paramsTicker = {'search':'PHM.BMEX', 'exchange':'BMEX'}
+    # add the key
+    paramsTicker['access_key'] = key
+    urlTicker = 'http://api.marketstack.com/v1/tickers'
+    r = requests.get(urlTicker,paramsTicker)
+    print(r.text)
 
-urlTicker = 'http://api.marketstack.com/v1/tickers'
+def eod(symbol):
+    params = {'symbols':symbol, 'exchange':'BMEX'}
+    # add the key
+    params['access_key'] = key
+    url = 'http://api.marketstack.com/v1/eod/latest'
+    r = requests.get(url,params)
+    print(r.text)
 
-r = requests.get(urlTicker,paramsTicker)
-
-print(r.text)
+if __name__ == "__main__":
+       #tickers()
+       eod('PHM.BMEX')
+    
+    
+    
+    
